2ade2c0b62c9fcd562379a19eb44b24bb5150b33
Phi Long Lê <20520621@gm.uit.edu.vn>
2022-05-02 15:45:28 +0700

    Thêm giao diện quên mật khẩu

--- ClinicManagement/Program.cs
+++ ClinicManagement/Program.cs
@@ -16,7 +16,7 @@ namespace ClinicManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new QuenMatKhau());
         }
     }
 }
